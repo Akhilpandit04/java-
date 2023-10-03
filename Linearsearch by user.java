d10bdeb7f2566ae4ae9abe22d80f806882fc36dd
Akhilpandit04 <144096882+Akhilpandit04@users.noreply.github.com>
2023-10-03 12:22:09 +0530

    Add files via upload

---
+++ Linearsearch by user.java
@@ -0,0 +1,29 @@
+
+//WAP to demonstrate passing of single dimeansional array to a function to perform linear search.
+import java.util.*;
+public class Linearsearch{
+public static int linearsearch(int key, int []a){
+        for(int i=0;i<a.length;i++){
+            if(a[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]){
+        int n,val;
+        Scanner sc=new Scanner(System.in);
+   System.out.print("Enter the range of an array:    ");
+   n=sc.nextInt();
+   int a1[]=new int[n];
+   System.out.print("enter the elements of array: ");
+   for(int i=0;i<n;i++){
+       a1[i]=sc.nextInt();
+       
+   }
+   System.out.print("enter the value to be searched: ");
+   val=sc.nextInt();
+   System.out.println(val+" is found at index: "+linearsearch(val,a1));
+    }
+}
