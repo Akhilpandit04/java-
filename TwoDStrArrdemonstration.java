eb1dd2c72f8db15bc98b3dde9f6d30d4017b69f6
Akhilpandit04 <144096882+Akhilpandit04@users.noreply.github.com>
2023-10-05 12:22:00 +0530

    Add files via upload

---
+++ TwoDStrArrdemonstration.java
@@ -0,0 +1,8 @@
+class TwoDStrArrdemonstration
+{
+public static void main(String[] args){
+   String[][] salutation={{"Mr.","Mrs.","Ms."},{"Kumar"}};
+    System.out.println(salutation[0][0]+" "+salutation[1][0]);
+     System.out.println(salutation[0][1]+salutation[1][0]);
+}
+}
